794b7471d27dcf79b65fd7cfa3056a7c4827f677
Dylan Hunt <dylan@imperium42.com>
2023-06-23 18:01:54 +0800

    doc: Better spacing

--- .hathora/utils/RunWslLinuxServerWinClients.sh
+++ .hathora/utils/RunWslLinuxServerWinClients.sh
@@ -6,12 +6,12 @@
 # -batchmode is useful, but it's hard to know when to -quit (or need to end task)
 # -nographics (combined with -batchmode) runs as headless dedicated server
 ##################################################################################
-# PHOTON FUSION BR CMDS
+# PHOTON-FUSION-BR CMDS
 # -dedicatedServer
 # -deathmatch
-# -scene GenArea2 # 2 == smallest
+# -scene GenArea2 // #2 == smallest
 #
-# More @ https://doc.photonengine.com/fusion/current/game-samples/fusion-br/quickstart
+# -> More @ https://doc.photonengine.com/fusion/current/game-samples/fusion-br/quickstart
 ##################################################################################
 # Clear the console
 clear
@@ -27,5 +27,13 @@ exe_name="Hathora-Unity-LinuxServer.x86_64"
 path_to_linux_server="../../Build-Server/$exe_name"
 echo "Starting $exe_name instance @ '$path_to_linux_server' ..."
 
-linux_cmd="$path_to_linux_server -batchmode -nographics -dedicatedServer -deathmatch -maxPlayers 5 -scene GenArea2 -region us -serverName LocalHeadlessServer -port 7777"
+linux_cmd="$path_to_linux_server -batchmode -nographics \
+	-dedicatedServer \
+	-deathmatch \
+	-maxPlayers 5 \
+	-scene GenArea2 \
+	-region us \
+	-serverName LocalHeadlessServer \
+	-port 7777"
+
 $linux_cmd
