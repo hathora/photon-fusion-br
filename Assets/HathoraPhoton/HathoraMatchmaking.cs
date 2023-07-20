6557a0896ace41b1f5700537194ebe47593c3088
Dylan Hunt <dylan@imperium42.com>
2023-07-20 15:03:14 +0800

    fix: Menu create flow

--- Assets/HathoraPhoton/HathoraMatchmaking.cs
+++ Assets/HathoraPhoton/HathoraMatchmaking.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json;
 using TMPro;
 using TPSBR;
 using UnityEngine;
+using UnityEngine.UI;
 using HathoraRegion = Hathora.Cloud.Sdk.Model.Region;
 using Assert = UnityEngine.Assertions.Assert;
 using Debug = UnityEngine.Debug;
@@ -21,7 +22,9 @@ namespace HathoraPhoton
         [Header("Pre-Create")]
         [SerializeField]
         private TextMeshProUGUI hathoraCreateStatusTxt;
-    
+        [SerializeField]
+        private Button createLobbyBtn;
+        
         [Header("Post-Create")]
         [SerializeField]
         private TextMeshProUGUI hathoraCreateDoneStatusTxt;
@@ -34,7 +37,7 @@ namespace HathoraPhoton
         private static HathoraPhotonClientMgr clientMgr => HathoraPhotonClientMgr.Singleton;
 
     
-        #region Base
+        #region Base Overrides
         protected override void OnAwake()
         {
             base.OnAwake();
@@ -56,41 +59,60 @@ namespace HathoraPhoton
             {
                 // ##############################################################
                 // "Host" Acting as a Hathora Client:
+                //
                 // 1. Ensure authed; already via HathoraPhotonClientMgr.Awake()
                 // 2. Create Lobby (a Room with server browsing capabilities)
-                // 3. Hide the modal create settings window and show success status
+                // 3. Wait for Lobby to be created
+                // 4. Hide the modal create settings window and show success status
+                //
                 // You should now be able to see the lobby within the list
                 // ##############################################################
                 await createHathoraSessionAsync(request);
             }
         }
-        #endregion // Base
+        #endregion // Base Overrides
     
 
         /// <summary>High-level, async Task wrapper for CreateSession</summary>
         /// <param name="_request"></param>
         private async Task createHathoraSessionAsync(SessionRequest _request)
         {
+            // Photon bypasses the Button interactable check 
+            if (!createLobbyBtn.interactable)
+                return;
+            
             string logPrefix = $"[HathoraMatchmaking.{nameof(createHathoraSessionAsync)}]";
             Debug.Log($"{logPrefix} (as Photon 'Host')");
         
-            // 1. Ensure authed; already via HathoraPhotonClientMgr.Awake()
+            // Ensure authed; already via HathoraPhotonClientMgr.Awake()
             if (!clientMgr.HathoraClientSession.IsAuthed)
             {
                 Debug.Log($"{logPrefix} !IsAuthed");
                 return;
             }
+
+            toggleCreateUi(_isCreating: true);
         
-            // 2. Create Lobby (a Room with server browsing capabilities)
+            // Create Lobby (a Room with server browsing capabilities)
             Lobby lobby = await photonHostCreateHathoraLobbyAsync(_request);
             Assert.IsNotNull(lobby?.RoomId, $"{logPrefix} Expected Lobby?.RoomId");
+            
+            // Wait for Lobby to be created
+            ConnectionInfoV2 connectionInfo = await clientMgr.GetActiveConnectionInfo(lobby.RoomId);
+            // IPAddress ip = await HathoraUtils.ConvertHostToIpAddress(connectionInfo.ExposedPort.Host);
 
-            // 3. Hide the modal create settings window and show success status
+            // Hide the modal create settings window and show success status
             onCreateLobbySuccessUI(lobby);
         }
 
+        private void toggleCreateUi(bool _isCreating)
+        {
+            hathoraCreateStatusTxt.gameObject.SetActive(_isCreating);
+            createLobbyBtn.gameObject.SetActive(!_isCreating);
+        }
+
         /// <summary>
-        /// 3. Hide the modal create settings window and show success status
+        /// Hide the modal create settings window and show success status
         /// </summary>
         private void onCreateLobbySuccessUI(Lobby _lobby)
         {
@@ -99,6 +121,8 @@ namespace HathoraPhoton
             hathoraCreateDoneStatusTxt.text = $"Created Lobby: {_lobby.RoomId}";
             hathoraCreateDoneStatusTxt.gameObject.SetActive(true);
             createSettingsModalPnl.SetActive(false);
+            
+            toggleCreateUi(_isCreating: false);
         }
 
         /// <summary>
